d0b11ce14cc5fe198f52993c302277205ea8b66b
OLIVIA KUO <o@OLIVIAs-MacBook-Pro.local>
2020-01-31 00:23:30 -0800

    Checkpoint 23 interfaces: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -12,4 +12,29 @@
       const nextSong = album.songs[nextSongIndex];
       player.playPause(nextSong);
   });
+    $('button#previous').on('click', function() {
+      if (player.playState !== 'playing') { return; }
+      const CURRENT_SONG_INDEX = album.songs.indexOf(player.currentlyPlaying);
+      const PREVIOUS_SONG_INDEX = CURRENT_SONG_INDEX - 1;
+      if (PREVIOUS_SONG_INDEX < 0) { return; }
+
+      const PREVIOUS_SONG = album.songs[PREVIOUS_SONG_INDEX];
+      player.playPause(PREVIOUS_SONG);
+  });
+
+    $('#time-control input').on('input', function(event) {
+      player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control input').val(percent);
+     }, 1000);
+     $('#time-control .current-time').text( currentTime );
+     $('#time-control input').on('input', function (event) {
+       player.skipTo(event.target.value);
+   });
 }
